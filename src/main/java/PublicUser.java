aa9b213f7f04ef35783aa965bf91c9e5c4b9835a
Ronan Mascarenhas <ronansm2@gmail.com>
2020-02-29 21:13:20 +0000

    added public user class

---
+++ src/main/java/PublicUser.java
@@ -0,0 +1,19 @@
+//package src.main.java;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class PublicUser {
+    private int id;
+
+}
+
+public int getId() {
+    return id;
+}
+
+public void setId(int id) {
+    this.id = id;
+}
