e321f7941d51f006e2d2888077f14b5fe57fcca7
Ethan <ethan.cairney@ucdconnect.ie>
2020-03-01 23:14:31 +0000

    Updated LoginController

--- src/main/java/lms/LoginController.java
+++ src/main/java/lms/LoginController.java
@@ -17,4 +17,32 @@ public class LoginController {
     @Autowired
     private UserRepository userRepository;
 
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("title", "BlogIt: Login");
+        if (userSession.isLoginFailed()) {
+            model.addAttribute("error", "Username and Password not correct");
+            userSession.setLoginFailed(false);
+        }
+        return "login.html";
+    }
+
+    @GetMapping("/logout")
+    public void logout(HttpServletResponse response) throws Exception {
+        userSession.setUser(null);
+        response.sendRedirect("/");
+    }
+
+    @PostMapping("/login")
+    public void doLogin(String username, String password, HttpServletResponse response) throws Exception {
+        Optional<User> user = userRepository.findByUsernameAndPassword(username, password);
+        if (user.isPresent()) {
+            userSession.setUser(user.get());
+            response.sendRedirect("/");
+        } else {
+            userSession.setLoginFailed(true);
+            response.sendRedirect("/login");
+        }
+
+    }
 }
