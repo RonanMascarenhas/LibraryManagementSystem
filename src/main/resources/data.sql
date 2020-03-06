f5d7a6498e591c2d0b148062c8cf3c1b9ead6d64
Fon111 <finn.oneill@ucdconnect.ie>
2020-03-06 16:46:59 +0000

    Index Changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-INSERT INTO artifact(id, name) VALUES (1, 'Mel is Love Mel is Life'), (2, 'I Love OOP');
-INSERT INTO user(id, username, password, role) VALUES (0, 'finn', 'password', 'librarian');
-=======
 INSERT INTO artifact(id, name) VALUES (10, 'Mel is Love Mel is Life'), (11, 'I Love OOP');
 INSERT INTO user(id, username, password, role) VALUES (10, 'finn', 'password', 'librarian'), (11, 'ethan', 'password', 'member');
->>>>>>> 590c5088b9465ad86f891322a7cdc78828e02338
